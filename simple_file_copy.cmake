e2177c091270042375dbccb61728b5da3c10ef2c
Trevor Cash <trevor@imatest.com>
2024-10-09 08:49:44 -0600

    change log level of simple_file_copy

--- simple_file_copy.cmake
+++ simple_file_copy.cmake
@@ -2,5 +2,5 @@
 
 cmake_path(GET CMAKE_ARGV5 PARENT_PATH parentPath)
 cmake_path(GET CMAKE_ARGV5 FILENAME filename)
-message("Copying file ${CMAKE_ARGV4} to ${parentPath}/${filename}")
+message(VERBOSE "Copying file ${CMAKE_ARGV4} to ${parentPath}/${filename}")
 file(COPY ${CMAKE_ARGV4} DESTINATION ${parentPath})
